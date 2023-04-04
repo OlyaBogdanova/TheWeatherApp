02b725bd5acca78c6487a27fb954c512eeb30575
OlyaBogdanova <olyavasilenko1992@gmail.com>
2023-04-04 08:57:21 +0300

    first commit

---
+++ src/assets/icons/indicators/IndicatorSvgSelector.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const InducatorSvgSelector = (props: Props) => {
+    return (
+        <div> InducatorSvgSelector</div>
+    )
+}
+
+export default InducatorSvgSelector
---
+++ src/pages/MonthStatistics/MonthStatistics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from '../Home/Home.module.scss';
+
+type Props = {}
+
+const Home = (props: Props) => {
+    return (
+        <div className={s.home}> Home</div>
+    )
+}
+
+export default Home
